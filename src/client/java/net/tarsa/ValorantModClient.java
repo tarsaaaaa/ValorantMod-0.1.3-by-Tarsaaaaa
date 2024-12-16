79b4897d115df20f0946aca445e5b2a253da9486
tarsaaaaa <tarsaaaaa1624@gmail.com>
2024-12-16 22:46:35 +0545

    -

---
+++ src/client/java/net/tarsa/ValorantModClient.java
@@ -0,0 +1,10 @@
+package net.tarsa;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ValorantModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/net/tarsa/ValorantModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.tarsa;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ValorantModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
