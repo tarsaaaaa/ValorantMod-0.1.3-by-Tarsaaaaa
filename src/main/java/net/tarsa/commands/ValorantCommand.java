f2a910eeff4828cb8436fa3449ecd478624fa7a9
tarsaaaaa <tarsaaaaa1624@gmail.com>
2024-12-23 22:19:29 +0545

    -

--- src/main/java/net/tarsa/commands/ValorantCommand.java
+++ src/main/java/net/tarsa/commands/ValorantCommand.java
@@ -3,17 +3,11 @@ package net.tarsa.commands;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
-import net.minecraft.nbt.NbtCompound;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
 import net.tarsa.components.GameHandler;
-import net.tarsa.interfaces.PlayerGameStatExt;
-
-import java.util.Objects;
-
-import static net.tarsa.util.ServerRegistry.*;
 
 public class ValorantCommand {
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
@@ -27,8 +21,14 @@ public class ValorantCommand {
                                 .executes(ValorantCommand::joinWithID)))//(/valorant join "string id")
                 .then(CommandManager.literal("leave")
                         .executes(ValorantCommand::leave))
+                .then(CommandManager.literal("create")
+                        .executes(ValorantCommand::createWithoutID)
+                        .then(CommandManager.argument("create-id", StringArgumentType.string())
+                                .executes(ValorantCommand::createWithID)))
         );
     }
+
+
 
     private static int leave(CommandContext<ServerCommandSource> context) {
         ServerCommandSource source = context.getSource();
@@ -41,18 +41,49 @@ public class ValorantCommand {
         return 0;
     }
 
-    private static int joinWithID(CommandContext<ServerCommandSource> context) {
-        return 0;
-    }
-
     private static int joinWithoutID(CommandContext<ServerCommandSource> context) {
         ServerCommandSource source = context.getSource();
         ServerPlayerEntity player = context.getSource().getPlayer();
         if (player == null) {
-            source.sendFeedback(() -> Text.translatable("command.valorant.join.error.notPlayer"), false);
+            source.sendFeedback(() -> Text.translatable("command.valorant.create.error.not-player"), false);
             return 1;
         }
-        GameHandler.joinRandomGame(player);
+        GameHandler.joinRandomGame(player, source);
+        return 0;
+    }
+
+    private static int joinWithID(CommandContext<ServerCommandSource> context) {
+        ServerCommandSource source = context.getSource();
+        ServerPlayerEntity player = context.getSource().getPlayer();
+        String gameId = StringArgumentType.getString(context, "join-id");
+        if (player == null) {
+            source.sendFeedback(() -> Text.translatable("command.valorant.create.error.not-player"), false);
+            return 1;
+        }
+        GameHandler.joinGameWithId(player, source, gameId);
+        return 0;
+    }
+
+    private static int createWithoutID(CommandContext<ServerCommandSource> context) {
+        ServerCommandSource source = context.getSource();
+        ServerPlayerEntity player = context.getSource().getPlayer();
+        if (player == null) {
+            source.sendFeedback(() -> Text.translatable("command.valorant.create.error.not-player"), false);
+            return 1;
+        }
+        GameHandler.createGameWithRandomID(player, source);
+        return 0;
+    }
+
+    private static int createWithID(CommandContext<ServerCommandSource> context) {
+        ServerCommandSource source = context.getSource();
+        ServerPlayerEntity player = context.getSource().getPlayer();
+        String gameId = StringArgumentType.getString(context, "create-id");
+        if (player == null) {
+            source.sendFeedback(() -> Text.translatable("command.valorant.create.error.not-player"), false);
+            return 1;
+        }
+        GameHandler.createGameWithGivenId(player, source, gameId);
         return 0;
     }
 
--- src/main/java/net/tarsa/components/GameHandler.java
+++ src/main/java/net/tarsa/components/GameHandler.java
@@ -1,26 +1,120 @@
 package net.tarsa.components;
 
 
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
-import org.apache.logging.log4j.core.jmx.Server;
 
+import java.util.List;
 import java.util.Objects;
 import java.util.Random;
 
 //Logic for the game.
 public class GameHandler {
-    //joins the player to a random game, tries* 10 times if couldn't the each.
-    public static void joinRandomGame(ServerPlayerEntity player) {
+    //creates a new game.
+    /*
+    * * filename = gameID *
+    * current-players=
+    * player-1=
+    * . . .
+    * player-n=
+    */
+    public static void createGame(String gameID, MinecraftServer server) {
+        ExternalFileHandler.writeFile("current-players=0\nplayer-0=\nplayer-1=\nplayer-2=\nplayer-3=\nplayer-4=\nplayer-5=\nplayer-6=\nplayer-7=\nplayer-8=\nplayer-9=", "valorant", gameID + ".txt", false, server);
+    }
+
+    //adds the player to the game with gameID.
+    public static void addPlayersToGame(String gameID, ServerPlayerEntity player, MinecraftServer server) {
+        int currentPlayers = Integer.parseInt(Objects.requireNonNull(ExternalFileHandler.readFile(1, "valorant", gameID + ".txt", "current-players", server)));
+        String playerKey = "player-" + (currentPlayers);
+        int newPlayers = currentPlayers + 1;
+        ExternalFileHandler.modifyFile(currentPlayers+2, playerKey, player.getName().getString(), "valorant", gameID + ".txt", server);
+        ExternalFileHandler.modifyFile(1, "current-players", String.valueOf(newPlayers), "valorant", gameID + ".txt", server);
+    }
+
+    //creates a game with random id.
+    public static void createGameWithRandomID(ServerPlayerEntity player, ServerCommandSource commandSource) {
         PlayerStats playerStats = new PlayerStats(player);
         if (!Objects.equals(playerStats.getPlayerGameID(), "")) {
-            player.sendMessage(Text.translatable("command.valorant.join.error.alreadyInGame"), false);
+            player.sendMessage(Text.translatable("command.valorant.create.error.already-in-game"), false);
+            return;
+        }
+        MinecraftServer server = commandSource.getServer();
+        String NewGameID = GameHandler.generateRandomID();
+        createGame(NewGameID, server);
+        addPlayersToGame(NewGameID, player, server);
+        playerStats.setPlayerGameID(NewGameID);
+        GlobalGameStats.setCurrentIDs(NewGameID);
+        player.sendMessage(Text.translatable("command.valorant.create.successfully-created"), false);
+    }
+    public static void createGameWithRandomID(ServerPlayerEntity player) {
+        PlayerStats playerStats = new PlayerStats(player);
+        if (!Objects.equals(playerStats.getPlayerGameID(), "")) {
+            player.sendMessage(Text.translatable("command.valorant.create.error.already-in-game"), false);
             return;
         }
         String NewGameID = GameHandler.generateRandomID();
         playerStats.setPlayerGameID(NewGameID);
         GlobalGameStats.setCurrentIDs(NewGameID);
-        player.sendMessage(Text.translatable("command.valorant.join.successfullyJoined"), false);
+        player.sendMessage(Text.translatable("command.valorant.create.successfully-created"), false);
+    }
+
+    //creates a game with given id.
+    public static void createGameWithGivenId(ServerPlayerEntity player, ServerCommandSource commandSource, String gameID) {
+        PlayerStats playerStats = new PlayerStats(player);
+        if (!Objects.equals(playerStats.getPlayerGameID(), "")) {
+            player.sendMessage(Text.translatable("command.valorant.create.error.already-in-game"), false);
+            return;
+        }
+        MinecraftServer server = commandSource.getServer();
+        List<String> inUseIds = GlobalGameStats.getCurrentIDs();
+        if (inUseIds != null) {
+            for (String id : inUseIds) {
+                if (Objects.equals(id, gameID)) {
+                    player.sendMessage(Text.translatable("command.valorant.create.error.id-already-in-use"), false);
+                    return;
+                }
+            }
+        }
+        createGame(gameID, server);
+        addPlayersToGame(gameID, player, server);
+        GlobalGameStats.setCurrentIDs(gameID);
+        player.sendMessage(Text.translatable("command.valorant.create.successfully-created"), false);
+    }
+
+    //join a random game.
+    public static void joinRandomGame(ServerPlayerEntity player, ServerCommandSource commandSource) {
+        List<String> inUseIds = GlobalGameStats.getCurrentIDs();
+        MinecraftServer server = commandSource.getServer();
+        final Random RANDOM = new Random();
+        int count = 0;
+        if (inUseIds != null) {
+            for (String id : inUseIds) {
+                count++;
+            }
+            int randomNo = RANDOM.nextInt(count);
+            String SelectedId = inUseIds.get(randomNo-1);
+            addPlayersToGame(SelectedId, player, server);
+            player.sendMessage(Text.translatable("command.valorant.join.no-id.successfully-joined"), false);
+            return;
+        }
+        createGameWithRandomID(player, commandSource);
+        player.sendMessage(Text.translatable("command.valorant.join.no-id.id-not-found-create-new"), false);
+    }
+
+    //join a game with given id.
+    public static void joinGameWithId(ServerPlayerEntity player, ServerCommandSource commandSource, String gameId) {
+        List<String> inUseIds = GlobalGameStats.getCurrentIDs();
+        MinecraftServer server = commandSource.getServer();
+        for (String id : inUseIds) {
+            if (Objects.equals(gameId, id)) {
+                addPlayersToGame(gameId, player, server);
+                player.sendMessage(Text.translatable("command.valorant.join.id.successfullyJoined"), false);
+                return;
+            }
+        }
+        player.sendMessage(Text.translatable("command.valorant.join.id.game-not-found"), false);
     }
 
     //gives the player's game info (id, )
@@ -34,6 +128,7 @@ public class GameHandler {
         player.sendMessage(Text.translatable("command.valorant.query.success", GameID), false);
     }
 
+    //attempts a leave operation on the player from the current game they are in.
     public static void leaveGame(ServerPlayerEntity player) {
         PlayerStats playerStats = new PlayerStats(player);
         if (Objects.equals(playerStats.getPlayerGameID(), "")) {
